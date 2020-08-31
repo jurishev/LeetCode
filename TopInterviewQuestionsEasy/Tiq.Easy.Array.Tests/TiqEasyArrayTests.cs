409e636bc83160561c288086b50a7db35d491c83
Yuri Shevchenko <jurishev@ya.ru>
2020-08-31 13:46:46 +0300

    Implement Remove Duplicates task

--- TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
@@ -4,6 +4,27 @@ namespace Tiq.Easy.Array.Tests
 {
     public class TiqEasyArrayTests
     {
+        [Theory]
+        [InlineData(null, 0, null)]
+        [InlineData(new int[] { }, 0, new int[] { })]
+        [InlineData(new int[] { 3 }, 1, new int[] { 3 })]
+        [InlineData(new int[] { 2, 2, 2 }, 1, new int[] { 2 })]
+        [InlineData(new int[] { 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2 }, 3, new int[] { 0, 1, 2 })]
+        [InlineData(new int[] { 1, 2, 3 }, 3, new int[] { 1, 2, 3 })]
+        public void RemoveDuplicatesTest(int[] nums, int expectedLength, int[] expectedSet)
+        {
+            var sln = new RemoveDuplicatesFromSortedArray();
+            var actualLength = sln.RemoveDuplicates(nums);
 
+            Assert.Equal(expectedLength, actualLength);
+
+            if (nums != null)
+            {
+                for (int i = 0; i < expectedSet.Length; i++)
+                {
+                    Assert.Equal(expectedSet[i], nums[i]);
+                }
+            }
+        }
     }
 }
---
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,24 @@
+﻿namespace Tiq.Easy.Array
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums) =>
+            nums is null || nums.Length == 0 ? 0 :
+            RemoveDuplicatesInPlace(nums);
+
+        private int RemoveDuplicatesInPlace(int[] nums)
+        {
+            var max = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[max])
+                {
+                    nums[++max] = nums[i];
+                }
+            }
+
+            return max + 1;
+        }
+    }
+}
