e70ef3fd4527472b71135c22f687e56c627d74ac
Yuri Shevchenko <jurishev@ya.ru>
2020-08-31 19:43:17 +0300

    Solve Intersection of Two Arrays problem

--- TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Tiq.Easy.Array.Tests
@@ -90,6 +91,54 @@ namespace Tiq.Easy.Array.Tests
         {
             var actual = new SingleNumberSolution().SingleNumberDictionary(nums);
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null, null, new int[] { })]
+        [InlineData(new int[] { }, new int[] { }, new int[] { })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 1, 2, 2, 2, 1 }, new int[] { 2, 2, 1 }, new int[] { 2, 2, 1 })]
+        [InlineData(new int[] { 1, 3, 3, 3, 2, 4 }, new int[] { 3, 3, 3 }, new int[] { 3, 3, 3 })]
+        [InlineData(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }, new int[] { 4, 9 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 6, 7, 3, 4, 5, 8, 9 }, new int[] { 3, 4, 5 })]
+        public void IntersectTest(int[] nums1, int[] nums2, int[] expected)
+        {
+            var actual = new IntersectionOfTwoArrays().Intersect(nums1, nums2);
+
+            var expDic = new Dictionary<int, int>();
+
+            foreach (var val in expected)
+            {
+                if (expDic.ContainsKey(val))
+                {
+                    expDic[val]++;
+                }
+                else
+                {
+                    expDic[val] = 1;
+                }
+            }
+
+            var actDic = new Dictionary<int, int>();
+
+            foreach (var val in actual)
+            {
+                if (actDic.ContainsKey(val))
+                {
+                    actDic[val]++;
+                }
+                else
+                {
+                    actDic[val] = 1;
+                }
+            }
+
+            Assert.Equal(expDic.Count, actDic.Count);
+
+            foreach (var key in expDic.Keys)
+            {
+                Assert.Equal(expDic[key], actDic[key]);
+            }
         }
     }
 }
---
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array/IntersectionOfTwoArrays.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tiq.Easy.Array
+{
+    public class IntersectionOfTwoArrays
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            if (nums1 is null || nums2 is null)
+            {
+                return System.Array.Empty<int>();
+            }
+
+            var small = nums1.Length < nums2.Length ? nums1 : nums2;
+            var large = nums1.Length < nums2.Length ? nums2 : nums1;
+
+            return GetIntersection(large, GetIndex(small)).ToArray();
+        }
+
+        private Dictionary<int, int> GetIndex(int[] arr)
+        {
+            var dic = new Dictionary<int, int>();
+
+            foreach (var val in arr)
+            {
+                if (dic.ContainsKey(val))
+                {
+                    dic[val]++;
+                }
+                else
+                {
+                    dic[val] = 1;
+                }
+            }
+
+            return dic;
+        }
+
+        private IEnumerable<int> GetIntersection(int[] arr, Dictionary<int, int> dic)
+        {
+            foreach (var val in arr)
+            {
+                if (dic.ContainsKey(val) && dic[val] > 0)
+                {
+                    yield return val;
+                    dic[val]--;
+                }
+            }
+        }
+    }
+}
