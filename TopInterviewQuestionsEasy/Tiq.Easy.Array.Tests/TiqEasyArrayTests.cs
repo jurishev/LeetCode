bb3e5816c5d04890ad8621bd7432b7982be9b0f2
Yuri Shevchenko <jurishev@ya.ru>
2020-08-31 13:55:52 +0300

    Implement Best Time To Buy And Sell task

--- TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
@@ -26,5 +26,19 @@ namespace Tiq.Easy.Array.Tests
                 }
             }
         }
+
+        [Theory]
+        [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 7)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4)]
+        [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+        [InlineData(new int[] { 2, 2, 2, 2, 2 }, 0)]
+        [InlineData(new int[] { 1 }, 0)]
+        public void MaxProfitTest(int[] prices, int expected)
+        {
+            var sln = new BestTimeToBuyAndSell();
+            var actual = sln.MaxProfit(prices);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
---
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array/BestTimeToBuyAndSell.cs
@@ -0,0 +1,34 @@
+﻿namespace Tiq.Easy.Array
+{
+    public class BestTimeToBuyAndSell
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var bought = false;
+            var profit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                var isLastDay = i == prices.Length - 1;
+                var shouldSell = isLastDay || prices[i] > prices[i + 1];
+                var shouldBuy = !isLastDay && prices[i] < prices[i + 1];
+
+                if (bought)
+                {
+                    if (shouldSell)
+                    {
+                        profit += prices[i];
+                        bought = false;
+                    }
+                }
+                else if (shouldBuy)
+                {
+                    profit -= prices[i];
+                    bought = true;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
