3f0e39554adc3c49bffe0d033a24356d3ec87fa8
Yuri Shevchenko <jurishev@ya.ru>
2020-09-01 12:08:31 +0300

    Solve Plus One problem

--- TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
@@ -140,5 +140,20 @@ namespace Tiq.Easy.Array.Tests
                 Assert.Equal(expDic[key], actDic[key]);
             }
         }
+
+        [Theory]
+        [InlineData(new int[] { 0 }, new int[] { 1 })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+        [InlineData(new int[] { 1, 2, 9 }, new int[] { 1, 3, 0 })]
+        [InlineData(new int[] { 9, 9, 9 }, new int[] { 1, 0, 0, 0 })]
+        public void PlusOneTest(int[] digits, int[] expected)
+        {
+            var actual = new PlusOneSolution().PlusOne(digits);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
     }
 }
---
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array/PlusOneSolution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Tiq.Easy.Array
+{
+    public class PlusOneSolution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var list = new List<int>(digits);
+            var more = false;
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if ((++list[i]) < 10)
+                {
+                    more = false;
+                    break;
+                }
+
+                more = true;
+                list[i] = 0;
+            }
+
+            if (more)
+            {
+                list.Insert(0, 1);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
