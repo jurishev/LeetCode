cbaef24b818eaee43e79e9d9ffe4cdbd33e74742
Yuri Shevchenko <jurishev@ya.ru>
2020-09-02 17:07:07 +0300

    Solve Rotate Image problem

--- TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array.Tests/TiqEasyArrayTests.cs
@@ -206,5 +206,126 @@ namespace Tiq.Easy.Array.Tests
 
         [Fact]
         public void IsValidSudokuTestTrue() => Assert.True(new ValidSudoku().IsValidSudoku(board));
+
+        private readonly int[][] matrix1input = new int[1][]
+        {
+            new int[] { 1 },
+        };
+
+        private readonly int[][] matrix1output = new int[1][]
+        {
+            new int[] { 1 },
+        };
+
+        [Fact]
+        public void RotateTestWidth1True()
+        {
+            new RotateImage().Rotate(matrix1input);
+            Assert.True(AreEqualMatrices(matrix1input, matrix1output));
+        }
+
+        private readonly int[][] matrix2input = new int[2][]
+        {
+            new int[] { 1, 2 },
+            new int[] { 3, 4 },
+        };
+
+        private readonly int[][] matrix2output = new int[2][]
+        {
+            new int[] { 3, 1 },
+            new int[] { 4, 2 },
+        };
+
+        [Fact]
+        public void RotateTestWidth2True()
+        {
+            new RotateImage().Rotate(matrix2input);
+            Assert.True(AreEqualMatrices(matrix2input, matrix2output));
+        }
+
+        private readonly int[][] matrix3input = new int[3][]
+        {
+            new int[] { 1, 2, 3 },
+            new int[] { 4, 5, 6 },
+            new int[] { 7, 8, 9 },
+        };
+
+        private readonly int[][] matrix3output = new int[3][]
+        {
+            new int[] { 7, 4, 1 },
+            new int[] { 8, 5, 2 },
+            new int[] { 9, 6, 3 },
+        };
+
+        [Fact]
+        public void RotateTestWidth3True()
+        {
+            new RotateImage().Rotate(matrix3input);
+            Assert.True(AreEqualMatrices(matrix3input, matrix3output));
+        }
+
+        private readonly int[][] matrix4input = new int[4][]
+        {
+            new int[] {  5,  1,  9, 11 },
+            new int[] {  2,  4,  8, 10 },
+            new int[] { 13,  3,  6,  7 },
+            new int[] { 15, 14, 12, 16 },
+        };
+
+        private readonly int[][] matrix4output = new int[4][]
+        {
+            new int[] { 15, 13,  2,  5 },
+            new int[] { 14,  3,  4,  1 },
+            new int[] { 12,  6,  8,  9 },
+            new int[] { 16,  7, 10, 11 },
+        };
+
+        [Fact]
+        public void RotateTestWidth4True()
+        {
+            new RotateImage().Rotate(matrix4input);
+            Assert.True(AreEqualMatrices(matrix4input, matrix4output));
+        }
+
+        private readonly int[][] matrix5input = new int[5][]
+        {
+            new int[] {  1,  2,  3,  4,  5 },
+            new int[] {  6,  7,  8,  9, 10 },
+            new int[] { 11, 12, 13, 14, 15 },
+            new int[] { 16, 17, 18, 19, 20 },
+            new int[] { 21, 22, 23, 24, 25 },
+        };
+
+        private readonly int[][] matrix5output = new int[5][]
+        {
+            new int[] { 21, 16, 11,  6,  1 },
+            new int[] { 22, 17, 12,  7,  2 },
+            new int[] { 23, 18, 13,  8,  3 },
+            new int[] { 24, 19, 14,  9,  4 },
+            new int[] { 25, 20, 15, 10,  5 },
+        };
+
+        [Fact]
+        public void RotateTestWidth5True()
+        {
+            new RotateImage().Rotate(matrix5input);
+            Assert.True(AreEqualMatrices(matrix5input, matrix5output));
+        }
+
+        private bool AreEqualMatrices(int[][] input, int[][] output)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (output[i][j] != input[i][j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
     }
 }
---
+++ TopInterviewQuestionsEasy/Tiq.Easy.Array/RotateImage.cs
@@ -0,0 +1,28 @@
+﻿namespace Tiq.Easy.Array
+{
+    public class RotateImage
+    {
+        public void Rotate(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length / 2; i++)
+            {
+                RotateCircle(matrix, i, matrix.Length - i - 1);
+            }
+        }
+
+        private void RotateCircle(int[][] matrix, int min, int max)
+        {
+            for (int i = min, j = max; i < max; i++)
+            {
+                var temp = matrix[i][min];
+
+                matrix[i][min] = matrix[max][i];
+                matrix[max][i] = matrix[j][max];
+                matrix[j][max] = matrix[min][j];
+                matrix[min][j] = temp;
+
+                j--;
+            }
+        }
+    }
+}
