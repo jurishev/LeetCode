2456ace119444970cb646ad81bc37438895188af
Yuri Shevchenko <jurishev@ya.ru>
2020-09-08 08:42:06 +0300

    Solve Convert Sorted Array to BST problem

---
+++ TopInterviewQuestionsEasy/Tiq.Easy.Trees/ConvertSortedArrayToBst.cs
@@ -0,0 +1,25 @@
+﻿namespace Tiq.Easy.Trees
+{
+    public class ConvertSortedArrayToBst
+    {
+        public TreeNode SortedArrayToBST(int[] nums) => Distribute(nums, 0, nums.Length - 1);
+
+        private TreeNode Distribute(int[] nums, int start, int end)
+        {
+            if (start > end)
+            {
+                return null;
+            }
+
+            var middle = (start + end) / 2;
+
+            var node = new TreeNode(nums[middle])
+            {
+                left = Distribute(nums, start, middle - 1),
+                right = Distribute(nums, middle + 1, end)
+            };
+
+            return node;
+        }
+    }
+}
