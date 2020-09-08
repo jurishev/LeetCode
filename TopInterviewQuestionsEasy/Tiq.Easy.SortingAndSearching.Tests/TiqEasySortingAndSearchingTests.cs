afc67a66d7199582f6b7ee85500cfe4ffafefda9
Yuri Shevchenko <jurishev@ya.ru>
2020-09-08 09:31:46 +0300

    Solve Merge Sorted Array problem

---
+++ TopInterviewQuestionsEasy/Tiq.Easy.SortingAndSearching.Tests/TiqEasySortingAndSearchingTests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Tiq.Easy.SortingAndSearching.Tests
+{
+    public class TiqEasySortingAndSearchingTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 4, 5, 6 }, 3, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(new int[] { 4, 5, 6, 0, 0, 0 }, 3, new int[] { 1, 2, 3 }, 3, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(new int[] { 1, 3, 5, 0, 0, 0 }, 3, new int[] { 2, 4, 6 }, 3, new int[] { 1, 2, 3, 4, 5, 6 })]
+        public void MergeTest(int[] nums1, int m, int[] nums2, int n, int[] expected)
+        {
+            new MergeSortedArray().Merge(nums1, m, nums2, n);
+            Assert.True(nums1.SequenceEqual(expected));
+        }
+    }
+}
---
+++ TopInterviewQuestionsEasy/Tiq.Easy.SortingAndSearching/MergeSortedArray.cs
@@ -0,0 +1,21 @@
+﻿namespace Tiq.Easy.SortingAndSearching
+{
+    public class MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = nums1.Length;
+
+            m--;
+            n--;
+
+            while (--i >= 0)
+            {
+                nums1[i] =
+                    m < 0 ? nums2[n--] :
+                    n < 0 ? nums1[m--] :
+                    nums1[m] >= nums2[n] ? nums1[m--] : nums2[n--];
+            }
+        }
+    }
+}
